eacf3b84b20dcaf4f7749cbc3d5ee95e5c7e87fd
wenlong.yu <1357035302@qq.com>
2020-01-17 00:04:22 +0800

    第一个启动案例

---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+/*
+路由器对象模块
+ */
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import MSite from '../pages/MSite/MSite.vue'
+import Search from '../pages/Search/Search.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+
+// 声明使用插件
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  // 所有路由
+  routes: [
+    {
+      path: '/msite',
+      component: MSite, // 返回路由组件的函数, 只有执行此函数才会加载路由组件, 这个函数在请求对应的路由路径时才会执行
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/order',
+      component: Order,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/profile',
+      component: Profile,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/',
+      redirect: '/msite'
+    }
+  ]
+})
