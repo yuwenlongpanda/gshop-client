0e53bab3794a007f5454777df977639109bade50
wenlong.yu <1357035302@qq.com>
2020-01-18 18:18:00 +0800

    FooterGuide 组件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="app">
     <router-view/>
     <FooterGuide/>
   </div>
@@ -15,5 +15,9 @@ export default {
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .app
+    width 100%
+    height 100%
+    background #f5f5f5
+    position relative
 </style>
