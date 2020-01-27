6f6db6c60c1a4a9f8c174d13ecde8b0348743352
wenlong.yu <1357035302@qq.com>
2020-01-27 20:02:36 +0800

    登录注册功能：切换显示或隐藏密码

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -31,10 +31,11 @@
                 <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
               </section>
               <section class="login_verification">
-                <input type="tel" maxlength="8" placeholder="密码">
-                <div class="switch_button off">
-                  <div class="switch_circle"></div>
-                  <span class="switch_text">...</span>
+                <input type="text" maxlength="8" placeholder="密码" v-model="pwd" v-if="showPwd">
+                <input type="password" maxlength="8" placeholder="密码" v-model="pwd" v-else>
+                <div class="switch_button" :class="showPwd?'on':'off'" @click="showPwd=!showPwd">
+                  <div class="switch_circle" :class="{right: showPwd}"></div>
+                  <span class="switch_text">{{showPwd ? 'abc' : '...'}}</span>
                 </div>
               </section>
               <section class="login_message">
@@ -59,8 +60,10 @@ export default {
   data () {
     return {
       loginWay: true, // 默认为true，代表手机验证码登录；false代码账号密码登录
-      phone: '',
-      computeTime: 0
+      showPwd: false, // 是否显示密码
+      computeTime: 0,
+      pwd: '',
+      phone: ''
     }
   },
   computed: {
@@ -190,6 +193,8 @@ export default {
                   background #fff
                   box-shadow 0 2px 4px 0 rgba(0,0,0,.1)
                   transition transform .3s
+                  &.right
+                    transform translateX(30px)
             .login_hint
               margin-top 12px
               color #999
