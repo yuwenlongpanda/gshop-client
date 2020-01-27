1010059f496c83c806598e2193e8894c006d910c
wenlong.yu <1357035302@qq.com>
2020-01-27 16:23:46 +0800

    登录注册功能：手机号合法检查

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -12,8 +12,8 @@
         <form>
           <div :class="{on: loginWay}">
             <section class="login_message">
-              <input type="tel" maxlength="11" placeholder="手机号">
-              <button disabled="disabled" class="get_verification">获取验证码</button>
+              <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
+              <button disabled="disabled" class="get_verification" :class="{right_phone: rightPhone}">获取验证码</button>
             </section>
             <section class="login_verification">
               <input type="tel" maxlength="8" placeholder="验证码">
@@ -56,7 +56,13 @@
 export default {
   data () {
     return {
-      loginWay: true // 默认为true，代表手机验证码登录；false代码账号密码登录
+      loginWay: true, // 默认为true，代表手机验证码登录；false代码账号密码登录
+      phone: ''
+    }
+  },
+  computed: {
+    rightPhone () {
+      return /^1\d{10}$/.test(this.phone)
     }
   }
 }
@@ -123,6 +129,8 @@ export default {
                 color #ccc
                 font-size 14px
                 background transparent
+                &.right_phone
+                  color black
             .login_verification
               position relative
               margin-top 16px
