974d0584b18692201d43813c99c2b6eff784060f
wenlong.yu <1357035302@qq.com>
2020-02-01 20:24:53 +0800

    项目优化/扩展（三）：图片懒加载: vue-lazyload

--- src/main.js
+++ src/main.js
@@ -8,8 +8,15 @@ import {Button} from 'mint-ui'
 
 import './mock/mockServer' // 加载mockServer即可
 
+import VueLazyload from 'vue-lazyload'
+import loading from './common/imgs/loading.gif'
+
 // 注册全局组件
 Vue.component(Button.name, Button)
+
+Vue.use(VueLazyload, {
+  loading
+})
 
 /* eslint-disable no-new */
 new Vue({
