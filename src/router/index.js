ae4207b6268704f10733bfaf3c54a089a24a199d
wenlong.yu <1357035302@qq.com>
2020-02-01 20:12:59 +0800

    项目优化/扩展（二）：路由组件懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,21 @@
  */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
-import MSite from '../pages/MSite/MSite.vue'
-import Search from '../pages/Search/Search.vue'
-import Order from '../pages/Order/Order.vue'
-import Profile from '../pages/Profile/Profile.vue'
 import Login from '../pages/Login/Login.vue'
 import Shop from '../pages/Shop/Shop.vue'
 import ShopGoods from '../pages/Shop/ShopGoods/ShopGoods.vue'
 import ShopRatings from '../pages/Shop/ShopRatings/ShopRatings.vue'
 import ShopInfo from '../pages/Shop/ShopInfo/ShopInfo.vue'
+
+// import MSite from '../pages/MSite/MSite.vue'
+// import Search from '../pages/Search/Search.vue'
+// import Order from '../pages/Order/Order.vue'
+// import Profile from '../pages/Profile/Profile.vue'
+
+const MSite = () => import('../pages/MSite/MSite.vue')
+const Search = () => import('../pages/Search/Search.vue')
+const Order = () => import('../pages/Order/Order.vue')
+const Profile = () => import('../pages/Profile/Profile.vue')
 
 // 声明使用插件
 Vue.use(VueRouter)
