1c25801334d8e35f84e53e4684133e3c10625fce
wenlong.yu <1357035302@qq.com>
2020-02-01 17:10:02 +0800

    开发Search 组件

--- src/pages/Search/Search.vue
+++ src/pages/Search/Search.vue
@@ -1,27 +1,89 @@
 <template>
   <section class="search">
-    <HeaderTop title="搜索">
-    </HeaderTop>
-    <form class="search_form" action="#">
-      <input type="search" name="search" placeholder="请输入商家或美食名称" class="search_input">
-      <input type="submit" name="submit" class="search_submit">
+    <HeaderTop title="搜索"/>
+    <form class="search_form"
+          @submit.prevent="search">
+      <input type="search"
+             placeholder="请输入商家名称"
+             class="search_input"
+             v-model="keyword">
+      <input type="submit"
+             class="search_submit">
     </form>
+    <section class="list"
+             v-if="!noSearchShops">
+      <ul class="list_container">
+        <!--:to="'/shop?id='+item.id"-->
+        <router-link :to="{path : '/shop', query : {id : item.id}}"
+                     tag="li"
+                     v-for="item in searchShops"
+                     :key="item.id"
+                     class="list_li">
+          <section class="item_left">
+            <img :src="item.imagePath"
+                 class="restaurant_img">
+          </section>
+          <section class="item_right">
+            <div class="item_right_text">
+              <p>
+                <span>{{item.name}}</span>
+              </p>
+              <p>月售 {{item.month_sales||item.recent_order_num}} 单</p>
+              <p>{{item.delivery_fee||item.float_minimum_order_amount}} 元起送 / 距离{{item.distance}}</p>
+            </div>
+          </section>
+        </router-link>
+      </ul>
+    </section>
+
+    <div class="search_none" v-else>很抱歉！无搜索结果</div>
   </section>
 </template>
 
 <script>
+import {mapState} from 'vuex'
 import HeaderTop from '../../components/HeaderTop/HeaderTop.vue'
 export default {
   components: {
     HeaderTop
+  },
+  data () {
+    return {
+      keyword: '',
+      noSearchShops: false
+    }
+  },
+  computed: {
+    ...mapState(['searchShops'])
+  },
+  watch: {
+    searchShops (value) {
+      if (!value.length) { // 没有数据
+        this.noSearchShops = true
+      } else { // 有数据
+        this.noSearchShops = false
+      }
+    }
+  },
+  methods: {
+    search () {
+      // 得到搜索关键字
+      const keyword = this.keyword.trim()
+      // 进行搜索
+      if (keyword) {
+        this.$store.dispatch('searchShops', keyword)
+      }
+    }
   }
 }
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus">
+<style lang="stylus" rel="stylesheet/stylus" scoped>
   @import "../../common/stylus/mixins.styl"
-  .search  //搜索
+  .search
     width 100%
+    height 100%
+    overflow hidden
     .search_form
       clearFix()
       margin-top 45px
@@ -47,4 +109,41 @@ export default {
           font-size 16px
           color #fff
           background-color #02a774
+
+    .list
+      .list_container
+        background-color: #fff;
+
+        .list_li
+          display: flex;
+          justify-content: center;
+          padding: 10px
+          border-bottom: 1px solid $bc;
+
+          .item_left
+            margin-right: 10px
+
+            .restaurant_img
+              width 50px
+              height 50px
+              display block
+
+          .item_right
+            font-size 12px
+            flex 1
+
+            .item_right_text
+              p
+                line-height 12px
+                margin-bottom 6px
+
+                &:last-child
+                  margin-bottom 0
+
+    .search_none
+      margin: 0 auto
+      color: #333
+      background-color: #fff
+      text-align: center
+      margin-top: 0.125rem
 </style>
